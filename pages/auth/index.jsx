a602756768516295d9071acd0c33f2590e361eda
Faris Chtatou <faris.manage@gmail.com>
2021-10-18 13:50:14 +0200

    Add login & register page

---
+++ pages/auth/index.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react"
+import { useRouter } from "next/router"
+import Link from "next/link"
+
+export default function Login() {
+  const router = useRouter()
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  })
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  function handleCredentialsChange(event) {
+    if (error) setError("")
+
+    setCredentials((state) => ({
+      ...state,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  return (
+    <div className="min-h-screen bg-white flex">
+      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div>
+            <img
+              className="h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+              Connectez-vous !
+            </h2>
+          </div>
+
+          <div className="mt-8">
+            <div className="mt-6">
+              <form method="POST" className="space-y-6" onSubmit={handleSubmit}>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Adresse mail
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      value={credentials.email}
+                      onChange={handleCredentialsChange}
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="space-y-1">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Mot de passe
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      value={credentials.password}
+                      onChange={handleCredentialsChange}
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <div className="text-sm">
+                    <Link href="/auth/register">
+                      <a className="font-small text-gray-500 hover:text-gray-700">
+                        Toujours pas inscrit ?
+                      </a>
+                    </Link>
+                  </div>
+
+                  <div className="text-sm">
+                    <Link href="/auth/password/forgot">
+                      <a className="font-medium text-indigo-600 hover:text-indigo-500">
+                        Mot de passe oublié ?
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    disabled={loading}
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
+                  >
+                    Se connecter
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden lg:block relative w-0 flex-1">
+        <img
+          className="absolute inset-0 h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+  )
+}
---
+++ pages/auth/register.jsx
@@ -0,0 +1,184 @@
+import { useState } from "react"
+import { useRouter } from "next/router"
+import Link from "next/link"
+
+export default function Login() {
+  const router = useRouter()
+  const [user, setUser] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  })
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  function handleChange(event) {
+    if (error) setError("")
+    setUser((state) => ({ ...state, [event.target.name]: event.target.value }))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  return (
+    <div className="min-h-screen bg-white flex">
+      <div className="hidden lg:block relative w-0 flex-1">
+        <img
+          className="absolute inset-0 h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+          alt=""
+        />
+      </div>
+      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div>
+            <img
+              className="h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+              Inscrivez-vous !
+            </h2>
+          </div>
+
+          <div className="mt-8">
+            <div className="mt-6">
+              <form method="POST" className="space-y-6" onSubmit={handleSubmit}>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Adresse mail
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="first_name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Prénom
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="first_name"
+                      name="first_name"
+                      type="text"
+                      autoComplete="first_name"
+                      required
+                      value={user.first_name}
+                      onChange={handleChange}
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="space-y-1">
+                  <label
+                    htmlFor="last_name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Nom
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="last_name"
+                      name="last_name"
+                      type="text"
+                      autoComplete="last_name"
+                      required
+                      value={user.last_name}
+                      onChange={handleChange}
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="space-y-1">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Mot de passe
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      value={user.password}
+                      onChange={handleChange}
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="space-y-1">
+                  <label
+                    htmlFor="repeatPassword"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Répétez votre mot de passe
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="repeatPassword"
+                      name="repeatPassword"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      value={user.repeatPassword}
+                      onChange={handleChange}
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <div className="text-sm">
+                    <Link href="/auth/">
+                      <a className="font-medium text-indigo-600 hover:text-indigo-500">
+                        Déjà inscrit ?
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    disabled={
+                      loading ||
+                      user.password !== user.repeatPassword ||
+                      user.password.length < 8
+                    }
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
+                  >
+                    S'inscrire
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
