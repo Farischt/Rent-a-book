d1dfc98fb09139bc2467e83c086f179d17ff89cb
Faris Chtatou <faris.manage@gmail.com>
2021-10-20 18:27:49 +0200

    Add new models

---
+++ server/models/Book.js
@@ -0,0 +1,80 @@
+import { Sequelize, DataTypes, Model, Op } from "sequelize"
+
+import slugify from "slugify"
+
+class Book extends Model {
+  setSlug() {
+    this.slug = slugify(this.title, { lower: true })
+  }
+
+  static async isConflict(title) {
+    return title &&
+      (await Book.findOne({
+        where: {
+          [Op.or]: [{ title }, { slug: slugify(title, { lower: true }) }],
+        },
+      }))
+      ? true
+      : false
+  }
+
+  static async getBySlug(slug) {
+    return (
+      slug &&
+      (await Book.findOne({
+        where: { slug },
+      }))
+    )
+  }
+}
+
+export default (sequelize, User, Asset) =>
+  Book.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      slug: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: null,
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+        references: {
+          model: User,
+          key: "id",
+        },
+      },
+
+      asset_id: {
+        type: Sequelize.UUID,
+        allowNull: true,
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+        references: {
+          model: Asset,
+          key: "id",
+        },
+      },
+    },
+    { sequelize, modelName: "Book" }
+  )
