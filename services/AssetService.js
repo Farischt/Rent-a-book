ccc416fc4f7f087a6f7d4323efd7ead6cf8fb963
Faris Chtatou <faris.manage@gmail.com>
2021-10-22 16:38:12 +0200

    Add methods signature change

--- services/AssetService.js
+++ services/AssetService.js
@@ -1,11 +1,11 @@
 import { API_BASE_URL } from "./services.config"
 
 class AssetApi {
-  getEncoded(assetId) {
+  getEncodedBookPicture(assetId) {
     return `${API_BASE_URL}/assets/books/${encodeURIComponent(assetId)}`
   }
 
-  getProfilePicture(assetId) {
+  getEncodedProfilePicture(assetId) {
     return `${API_BASE_URL}/assets/users/${encodeURIComponent(assetId)}`
   }
 }
