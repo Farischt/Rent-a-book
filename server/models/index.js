cbe08a14b9740b68642fcfcd91f1ae46d7ef0d33
Faris Chtatou <faris.manage@gmail.com>
2021-10-25 16:27:10 +0200

    Add database remodeling changes

--- server/models/index.js
+++ server/models/index.js
@@ -5,9 +5,11 @@ import AccountConfirmationTokenModel from "./AccountConfirmationToken"
 import PasswordResetTokenModel from "./PasswordResetToken"
 import BookModel from "./Book"
 import BookPictureModel from "./BookPicture"
+import LoanModel from "./Loan"
 
 export default (sequelize) => {
   const User = UserModel(sequelize)
+  const Book = BookModel(sequelize)
   const UserPicture = UserPictureModel(sequelize, User)
   const AccountConfirmationToken = AccountConfirmationTokenModel(
     sequelize,
@@ -15,16 +17,17 @@ export default (sequelize) => {
   )
   const PasswordResetToken = PasswordResetTokenModel(sequelize, User)
   const AuthToken = AuthTokenModel(sequelize, User)
-  const Book = BookModel(sequelize, User)
   const BookPicture = BookPictureModel(sequelize, Book)
+  const Loan = LoanModel(sequelize, User, Book)
 
   return {
     User,
+    Book,
     UserPicture,
     AccountConfirmationToken,
     PasswordResetToken,
     AuthToken,
-    Book,
     BookPicture,
+    Loan,
   }
 }
